1654a6734fdf2080c072fed92b64d049ec2f18f0
John <johnmichael.camara.git@gmail.com>
2026-09-24 00:51:26 +0800

    Fix proxy SSL headers for OAuth callback

--- config/settings.py
+++ config/settings.py
@@ -168,6 +168,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
